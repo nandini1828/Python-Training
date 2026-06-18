ebc8c8afd88bc373b3a21a2f28d3ccaee09ad033
Karthik-reddie <kusham@wscs.ai>
2026-06-18 12:25:50 +0530

    Updated oops.py with examples

--- day1/oops.py
+++ day1/oops.py
@@ -12,5 +12,54 @@ OOP is a way of designing software using Objects and Classes."""
 # 4. Polymorphism -> Ability to take many forms (same method name but different behavior)
 
 
+# ENCAPSULATION
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # Private variable
+
+    def get_balance(self):
+        return self.__balance
+account = BankAccount(1000)
+print(account.get_balance())
+
+# ABSTRACTION   
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+class Car(Vehicle):
+    def start(self):
+        print("Car Started")
+car = Car()
+car.start() 
+
+# INHERITANCE
+class Animal:
+    def eat(self):
+        print("Animal is eating")
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+    def bark(self):
+        print(f"{self.name} is barking")
+dog = Dog("Buddy")
+dog.eat()
+
+# POLYMORPHISM
+class Dog:
+    def sound(self):
+        print("Bark")
+class Cat:
+    def sound(self):
+        print("Meow")
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
+
+
+
 
 
