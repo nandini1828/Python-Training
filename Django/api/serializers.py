cc29b52901ba1399b15c5a764ae10224da33a03c
msig4 <msig4@msig4s-MacBook-Pro.local>
2026-07-01 13:09:59 +0530

    Updated the files in  Django folder

--- Django/api/serializers.py
+++ Django/api/serializers.py
@@ -5,6 +5,7 @@ from .models import Course, Enrollment, Student
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
+        
         model = Student
         fields = "__all__"
 
